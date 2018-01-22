5a55fbf1e549ffaf799d17458e0f4806a12e8177
r1ch <rich@bradi.sh>
2018-01-22 11:24:46 +0000

    Update payment-form-handler.js

--- js/payment-form-handler.js
+++ js/payment-form-handler.js
@@ -1,7 +1,36 @@
+var stripeKeys = {
+    live: "pk_live_5rRV2gicedvJNhurme4I8Gnd",
+    test: "pk_test_mq2ehqe1tkcP71ZFwCQHjqng"
+}
+  
+//Startup message
+console.log("Found Stripe Mode:",GLOBAL.stripeMode)
+  
+//initialise stripe
+var stripe = Stripe(stripeKeys[GLOBAL.stripeMode]);
+var elements = stripe.elements();
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+//angular form controller
 var paymentsApp = angular.module('paymentApp',[]);
 paymentsApp.controller('paymentFormController',['$scope','$http',function($scope,$http){
-  //start Stripe
-  var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+  //stripe keys - choose which one we want in Jekyll _config.yaml
+  
+  
+  //bind and idempotency key and description to the form
   $scope.idem = Math.random() + Date.now();
+  $scope.description = GLOBAL.description;
+  
+  
   window.scope = $scope
+  
 }])
