dab9954ba8f3548417744811da580debdadf2b46
r1ch <rich@bradi.sh>
2018-01-22 10:55:32 +0000

    Update payment-form-handler.js

--- js/payment-form-handler.js
+++ js/payment-form-handler.js
@@ -1,4 +1,6 @@
 var paymentsApp = angular.module('paymentsApp',[]);
-paymentsApp.controller('paymentsFormController',[$scope,$http,function($scope,$http){
+paymentsApp.controller('paymentsFormController',['$scope','$http',function($scope,$http){
+  //start Stripe
+  var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
   $scope.idem = Math.random() + Date.now();
 }])
